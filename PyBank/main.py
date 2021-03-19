65294f9f550d00e0994cee3afb3a80ae50143f38
dejayru <rkpmobile@gmail.com>
2021-03-19 09:22:20 -0400

    comments for code

---
+++ PyBank/main.py
@@ -0,0 +1,30 @@
+#PyBank Homework
+
+# Import as os and csv file
+
+# create path for csv file
+
+# create list for data
+
+# set variables
+
+# open the csv file to set path
+
+    #count the number of months in data file
+
+    #append the date
+
+    # profit infomation to calc total profit
+
+    #calc avg change in profits month to month
+
+    #create list of monthly changes
+
+    #calc the avg change month to month
+
+    #get max/min for change in profits and dates
+
+#analysis output
+
+#output as text
+
