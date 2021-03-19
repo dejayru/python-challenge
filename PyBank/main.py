4e72481825605876a5a33477f83f51f335069357
dejayru <rkpmobile@gmail.com>
2021-03-19 09:52:14 -0400

    code up to analysis output

--- PyBank/main.py
+++ PyBank/main.py
@@ -30,16 +30,37 @@ with open(bankcsv, 'r') as csvfile:
     #append the date
         date.append(row[0])
     # profit infomation to calc total profit
+        profit.append(row[1])
+        total_profit = total_profit + int(row[1])
 
     #calc avg change in profits month to month
-
+        final_profit = int(row[1])
+        month_changes_profits = final_profit -  inital_profit
     #create list of monthly changes
+        month_changes.append(month_changes_profits)
 
+        total_change_profits = total_change_profits + month_changes_profits
+        inital_profit = final_profit
     #calc the avg change month to month
+        average_change_profits = (total_change_profits/count)
 
     #get max/min for change in profits and dates
+        largest_increase_profits = max(month_changes)
+        largest_decrease_profits = min(month_changes)
+
+        Increase_date = date[month_changes.index(largest_increase_profits)]
+        decrease_date = date[month_changes.index(largest_decrease_profits)]
 
 #analysis output
-
+    print('.........................................')
+    print("Financial Analysis")
+    print('.........................................')
+    print("Total Months: " + str(count))
+    print("Total Profits: " + "$" + str(total_profit))
+    print("Average Change " + "$" +str(int(average_change_profits)))
+    print("Greatest Increase in Profits: " + str(Increase_date) + " ($" + str(largest_increase_profits) + ")")
+    print("Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(largest_decrease_profits) + ")")
+    print('.........................................')
+    
 #output as text
 
